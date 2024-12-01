6f867f7e719ddeac9e1c9719678f69998ee38ef3
Ale <156391201+AleRodriguezCruz@users.noreply.github.com>
2024-11-30 19:40:43 -0800

    Update ForgotPasswordScreen.js
    se agrego el diseño

--- screens/ForgotPasswordScreen.js
+++ screens/ForgotPasswordScreen.js
@@ -1,309 +1,183 @@
-//DeviceScreen.js
+// screens/ForgotPasswordScreen.js
+import React, { useState } from 'react';
+import { SafeAreaView, Text, TextInput, TouchableOpacity, StyleSheet, View, ImageBackground, Image } from 'react-native';
+import axios from 'axios'; // Asegúrate de importar Axios
 
-import React, { useState, useContext } from 'react';
-import {
-  StyleSheet,
-  SafeAreaView,
-  ScrollView,
-  View,
-  Text,
-  TouchableOpacity,
-  Switch,
-  Image,
-  Alert,
-  Linking
-} from 'react-native';
-import FeatherIcon from 'react-native-vector-icons/Feather';
-import * as ImagePicker from 'expo-image-picker';
-import { useNavigation, useRoute } from '@react-navigation/native'; // Importamos useNavigation
-import Icon from 'react-native-vector-icons/Ionicons';
-import { ImageContext } from '../screens/ImageContext';
+const ForgotPasswordScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
+  const handleResetPassword = async () => {
+    if (email && newPassword && confirmPassword) {
+      if (newPassword === confirmPassword) {
+        try {
+          // Realizar la solicitud POST a la API para restablecer la contraseña
+          const response = await axios.post('https://flaskrosetalummitechapi.vercel.app/api/reset-password', {
+            email: email,
+            new_password: newPassword,
+          }, {
+            headers: {
+              'accept': 'application/json',
+              'Content-Type': 'application/json',
+            },
+          });
 
-export default function DevicesScreen() {
-  const navigation = useNavigation(); // Obtenemos el objeto de navegación
-  const route = useRoute(); // Obtenemos la ruta para recibir parámetros
-  const { correo } = route.params || {}; // Extraemos el correo desde los parámetros
-
-  const { avatarSource, setAvatarSource } = useContext(ImageContext); // Usamos el contexto para manejar la imagen
-  const [form, setForm] = useState({
-    emailNotifications: true,
-    pushNotifications: false,
-  });
-
-  // Función para seleccionar imagen
-  const handleSelectImage = async () => {
-    const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
-
-    if (permissionResult.granted === false) {
-      Alert.alert("Permiso Denegado", "No puedes acceder a las fotos sin el permiso.");
-      return;
+          // Manejar la respuesta de la API
+          if (response.status === 200) {
+            alert('Contraseña restablecida con éxito.');
+            navigation.navigate('Login'); // Navegar de vuelta a la pantalla de inicio de sesión
+          } else {
+            alert('Error al restablecer la contraseña. Por favor intenta de nuevo.');
+          }
+        } catch (error) {
+          // Manejo del error
+          if (error.response) {
+            alert('Error: ' + error.response.data.message || 'Ocurrió un error al restablecer la contraseña.');
+          } else {
+            alert('Error: ' + error.message);
+          }
+        }
+      } else {
+        alert('Las contraseñas no coinciden.');
+      }
+    } else {
+      alert('Por favor completa todos los campos.');
     }
-
-    Alert.alert('Selecciona una opción', 'Elige cómo deseas seleccionar la imagen', [
-      {
-        text: 'Usar cámara',
-        onPress: () => handleLaunchCamera(),
-      },
-      {
-        text: 'Seleccionar de la galería',
-        onPress: () => handleLaunchImageLibrary(),
-      },
-      {
-        text: 'Cancelar',
-        style: 'cancel',
-      },
-    ]);
   };
-
-  // Función para manejar la selección de imagen desde la galería
- const handleLaunchImageLibrary = async () => {
-  const pickerResult = await ImagePicker.launchImageLibraryAsync({
-    mediaTypes: ImagePicker.MediaTypeOptions.Images,
-    allowsEditing: true,
-    quality: 1,
-  });
-
-  if (!pickerResult.canceled && pickerResult.assets?.length > 0) {
-    console.log('Image URI:', pickerResult.assets[0].uri); // Debug
-    setAvatarSource({ uri: pickerResult.assets[0].uri });
-  }
-};
-
-const handleLaunchCamera = async () => {
-  const cameraResult = await ImagePicker.launchCameraAsync({
-    allowsEditing: true,
-    quality: 1,
-  });
-
-  if (!cameraResult.canceled && cameraResult.assets?.length > 0) {
-    console.log('Camera Image URI:', cameraResult.assets[0].uri); // Debug
-    setAvatarSource({ uri: cameraResult.assets[0].uri });
-  }
-};
-
-// Función para manejar el cierre de sesión
-  const handleLogout = () => {
-    // Aquí puedes hacer lo necesario para cerrar sesión, por ejemplo, eliminar el token de autenticación
-    // o redirigir al usuario a la pantalla de inicio de sesión:
-    Alert.alert('Cerrar sesión', '¿Estás seguro de que deseas cerrar sesión?', [
-      {
-        text: 'Cancelar',
-        style: 'cancel',
-      },
-      {
-        text: 'Cerrar sesión',
-        onPress: () => {
-          navigation.replace('LoginScreen'); // Redirigir a la pantalla de login (ajusta el nombre de la pantalla según corresponda)
-        },
-      },
-    ]);
-  };
-
- // Función para manejar el redireccionamiento a la página de contacto
-  const handleContactUs = () => {
-    // Reemplaza esta URL con la página web de contacto que desees
-    const contactUrl = 'https://landing-page-kappa-cyan.vercel.app/';
-    Linking.openURL(contactUrl).catch((err) =>
-      console.error("Error al abrir la URL: ", err)
-    );
-  };
-
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
-      {/* Botón de regresar */}
-      <TouchableOpacity
-        style={styles.backButton}
-        onPress={() => navigation.goBack()} // Acción para regresar
-      >
-        <Icon name="arrow-back" size={30} color="#000" />
-      </TouchableOpacity>
-
-      <View style={styles.profile}>
-        <TouchableOpacity onPress={handleSelectImage}>
-          <View style={styles.profileAvatarWrapper}>
-            <Image
-              alt=""
-              source={avatarSource} // Usar el estado para la imagen
-              style={styles.profileAvatar}
-            />
-            <TouchableOpacity onPress={handleSelectImage}>
-              <View style={styles.profileAction}>
-                <FeatherIcon color="#fff" name="edit-3" size={15} />
-              </View>
-            </TouchableOpacity>
-          </View>
-        </TouchableOpacity>
-
-        <View>
-          <Text style={styles.profileName}>{correo || ""}</Text>
-
-        </View>
-      </View>
-
-      <ScrollView>
-        <View style={styles.section}>
-          <Text style={styles.sectionTitle}>Preferencias</Text>
-
-
-          <View style={styles.row}>
-            <View style={[styles.rowIcon, { backgroundColor: '#fff' }]}>
-              <FeatherIcon color="#000" name="at-sign" size={20} />
-            </View>
-            <Text style={styles.rowLabel}>Notificaciones-Email</Text>
-            <View style={styles.rowSpacer} />
-            <Switch
-              onValueChange={emailNotifications => setForm({ ...form, emailNotifications })}
-              value={form.emailNotifications}
+    <ImageBackground 
+      
+      style={styles.background}
+      resizeMode="cover"
+    >
+      <SafeAreaView style={styles.container}>
+        <View style={styles.forgotPasswordBox}>
+          <Image source={require('../assets/logo3.png')} style={styles.logo} />
+          <Text style={styles.title}>Recuperar Contraseña</Text>
+          
+          <View style={styles.inputBox}>
+            <Text style={styles.label}>Correo</Text>
+            <TextInput
+              style={[styles.input, { borderColor: '#90E0EF' }]}
+              placeholder="Correo"
+              value={email}
+              onChangeText={setEmail}
+              autoCapitalize="none"
+              placeholderTextColor="#aaa"
             />
           </View>
 
-          <View style={styles.row}>
-            <View style={[styles.rowIcon, { backgroundColor: '#fff' }]}>
-              <FeatherIcon color="#000" name="bell" size={20} />
-            </View>
-            <Text style={styles.rowLabel}>Notificaciones</Text>
-            <View style={styles.rowSpacer} />
-            <Switch
-              onValueChange={pushNotifications => setForm({ ...form, pushNotifications })}
-              value={form.pushNotifications}
+          <View style={styles.inputBox}>
+            <Text style={styles.label}>Nueva Contraseña</Text>
+            <TextInput
+              style={[styles.input, { borderColor: '#90E0EF' }]}
+              placeholder="Nueva Contraseña"
+              secureTextEntry
+              value={newPassword}
+              onChangeText={setNewPassword}
+              placeholderTextColor="#aaa"
             />
           </View>
+
+          <View style={styles.inputBox}>
+            <Text style={styles.label}>Confirmar Contraseña</Text>
+            <TextInput
+              style={[styles.input, { borderColor: '#90E0EF' }]}
+              placeholder="Confirmar Contraseña"
+              secureTextEntry
+              value={confirmPassword}
+              onChangeText={setConfirmPassword}
+              placeholderTextColor="#aaa"
+            />
+          </View>
+
+          <TouchableOpacity style={styles.button} onPress={handleResetPassword}>
+            <Text style={styles.buttonText}>Restablecer Contraseña</Text>
+          </TouchableOpacity>
+
+          <View style={styles.login}>
+            <Text style={{ color: '#000', textAlign: 'center' }}>
+              ¿Ya tienes tu contraseña? 
+              <Text 
+                style={styles.link} 
+                onPress={() => navigation.navigate('Login')} // Navegar a la pantalla de inicio de sesión
+              >
+                Inicia Sesión
+              </Text>
+            </Text>
+          </View>
         </View>
-
-        <View style={styles.section}>
-          <Text style={styles.sectionTitle}>Recursos</Text>
-
-          <TouchableOpacity onPress={() => { /* handle onPress */ }} style={styles.row}>
-            <View style={[styles.rowIcon, { backgroundColor: '#fff' }]}>
-              <FeatherIcon color="#000" name="flag" size={20} />
-            </View>
-            <Text style={styles.rowLabel}>Reportar Bug</Text>
-            <View style={styles.rowSpacer} />
-            <FeatherIcon color="#C6C6C6" name="chevron-right" size={20} />
-          </TouchableOpacity>
-
-          <TouchableOpacity onPress={handleContactUs} style={styles.row}>
-            <View style={[styles.rowIcon, { backgroundColor: '#fff' }]}>
-              <FeatherIcon color="#000" name="mail" size={20} />
-            </View>
-            <Text style={styles.rowLabel}>Contáctanos</Text>
-            <View style={styles.rowSpacer} />
-            <FeatherIcon color="#C6C6C6" name="chevron-right" size={20} />
-          </TouchableOpacity>
-
-          <TouchableOpacity onPress={() => { /* handle onPress */ }} style={styles.row}>
-            <View style={[styles.rowIcon, { backgroundColor: '#fff' }]}>
-              <FeatherIcon color="#000" name="star" size={20} />
-            </View>
-            <Text style={styles.rowLabel}>Calificar en App Store</Text>
-            <View style={styles.rowSpacer} />
-            <FeatherIcon color="#C6C6C6" name="chevron-right" size={20} />
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
-            <Text style={styles.logoutButtonText}>Cerrar Sesión</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+      </SafeAreaView>
+    </ImageBackground>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  /** Profile */
-  profile: {
-    padding: 24,
+  background: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: '#fff',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  profileAvatarWrapper: {
-    position: 'relative',
-    marginTop: 40,
+  forgotPasswordBox: {
+    width: 400,
+    padding: 20,
+    borderRadius: 15,
+    backgroundColor: '#fff',
+    alignItems:'center',
   },
-  profileAvatar: {
-    width: 72,
-    height: 72,
-    borderRadius: 9999,
+  logo: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
   },
-  profileAction: {
-    position: 'absolute',
-    right: -4,
-    bottom: -10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 28,
-    height: 28,
-    borderRadius: 9999,
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#000',
+    marginBottom: 15,
+  },
+  inputBox: {
+    width: '100%',
+    marginVertical: 10,
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#000',
+    marginBottom: 5,
+  },
+  input: {
+    height: 40,
+    borderColor:'#0077B6',
+    borderWidth: 2,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    color: '#000',
+    backgroundColor: '#90E0EF',
+    fontSize: 14,
+  },
+  button: {
+    width: '80%',
+    height: 40,
     backgroundColor: '#007bff',
-  },
-  profileName: {
-    marginTop: 20,
-    fontSize: 19,
-    fontWeight: '600',
-    color: '#414d63',
-    textAlign: 'center',
-  },
-  /** Section */
-  section: {
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    paddingVertical: 12,
-    fontSize: 12,
-    fontWeight: '600',
-    color: '#9e9e9e',
-    textTransform: 'uppercase',
-    letterSpacing: 1.1,
-  },
-  /** Row */
-  row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    height: 50,
-    backgroundColor: '#fff',
-    borderRadius: 8,
-    marginBottom: 12,
-    paddingHorizontal: 12,
-  },
-  rowIcon: {
-    width: 32,
-    height: 32,
-    borderRadius: 9999,
-    marginRight: 12,
-    flexDirection: 'row',
-    alignItems: 'center',
+    borderRadius: 20,
     justifyContent: 'center',
-  },
-  rowLabel: {
-    fontSize: 17,
-    fontWeight: '400',
-    color: '#0c0c0c',
-
-  },
-  rowSpacer: {
-    flexGrow: 1,
-    flexShrink: 1,
-    flexBasis: 0,
-  },
-  backButton: {
-    position: 'absolute',
-    top: 40,
-    left: 20,
-    zIndex: 1
-  },
-  /** Botón Cerrar Sesión */
-  logoutButton: {
-    backgroundColor: '#FF4C4C', // Color rojo para llamar la atención
-    paddingVertical: 15,
-    paddingHorizontal: 24,
-    borderRadius: 8,
     alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 20,
+    marginTop: 15,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 14,
+    fontWeight: '600',
   },
 });
+
+export default ForgotPasswordScreen;
